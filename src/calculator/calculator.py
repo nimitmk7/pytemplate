3b90b52cbdb875c77c41fb27789848b322fc00ed
Nimit Kanani <nimitmk7@gmail.com>
2025-04-04 22:03:09 -0400

    Ruff linting added and Calculator component in Progress

---
+++ src/calculator/calculator.py
@@ -0,0 +1,20 @@
+
+class Calculator:
+    
+    def add(self, a: float, b: float) -> float:
+        """Add two numbers and return the result."""
+        return a + b
+
+    def subtract(self, a: float, b: float) -> float:
+        """Subtract two numbers and return the result."""
+        return a - b
+
+    def multiply(self, a: float, b: float) -> float:
+        """Multiply two numbers, and return the result."""
+        return a * b
+
+    def divide(self, a:float, b:float) -> float:
+        """Divide two numbers, while making sure to avoid division by zero, and return the result."""
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
+        return a / b
