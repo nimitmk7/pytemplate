9e0ca84b6b74d82529b46d1f2f3a625da7166853
Nimit Kanani <nimitmk7@gmail.com>
2025-04-05 11:54:38 -0400

    Added Calc component and enabled CircleCI

---
+++ src/calculator/calculator_interface.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, a, b):
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    def subtract(self, a, b):
+        raise NotImplementedError("This method should be overridden by subclasses")
+
+    def multiply(self, a, b):
+       raise NotImplementedError("This method should be overridden by subclasses")
+
+    def divide(self, a, b):
+        raise NotImplementedError("This method should be overridden by subclasses")
